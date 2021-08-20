d80a88c086f6a372bce45e65fa44ebfbbb153273
Michael Ripley <zkxs00@gmail.com>
2021-08-19 22:16:23 -0500

    Initial commit

---
+++ NeosLocomotionRename/NeosLocomotionRename.cs
@@ -0,0 +1,48 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Reflection;
+
+namespace NeosLocomotionRename
+{
+    public class NeosLocomotionRename : NeosMod
+    {
+        public override string Name => "NeosLocomotionRename";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosLocomotionRename";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.michaelripley.NeosLocomotionRename");
+            MethodInfo patch = AccessTools.DeclaredMethod(typeof(NeosLocomotionRename), nameof(NeosLocomotionRename.GetLocomotionNamePostfix));
+
+            Type[] moduleTypes = new Type[]
+            {
+                typeof(LocomotionModule),
+                typeof(NoclipLocomotion),
+                typeof(GrabWorldLocomotion),
+                typeof(SlideLocomotion),
+                typeof(TeleportLocomotion),
+            };
+
+            foreach (Type moduleType in moduleTypes)
+            {
+                MethodInfo originalMethod = AccessTools.DeclaredPropertyGetter(moduleType, nameof(LocomotionModule.LocomotionName));
+                if (originalMethod == null)
+                {
+                    Warn($"Could not find {moduleType.Name}.LocomotionName getter, skipping!");
+                    break;
+                }
+                harmony.Patch(originalMethod, postfix: new HarmonyMethod(patch));
+                Msg($"{moduleType.Name} patch installed successfully");
+            }
+        }
+
+        private static void GetLocomotionNamePostfix(LocomotionModule __instance, ref LocaleString __result)
+        {
+            __result = __instance.Slot.Name;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# NeosLocomotionRename
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) makes your locomotion modules derive their names from their slots. This makes it easier to distinguish between multiple installed locomotion modules of the same type, e.g. "Noclip", "Fast Noclip" "6-Axis Noclip". Without the mod your context menu would instead show "Noclip", "Noclip", "Noclip".
+
+This will no longer be needed once https://github.com/Neos-Metaverse/NeosPublic/issues/1653 is resolved.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [NeosLocomotionRename.dll](https://github.com/zkxs/NeosLocomotionRename/releases/latest/download/NeosLocomotionRename.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+## FAQ
+### Will this cause any weird behavior?
+Yes. You may experience the following:
+- Incorrect default locomotion module upon joining worlds
+- Items that switch your locomotion module will behave less reliably, and they were *already* unreliable.
+
+### What does this actually do?
+Forces Noclip, GrabWorld, Slide, and Teleport locomotion modules to use the slot name as the module name. Physical is specifically exempted because it has a built in mechanism for renaming.
+
+### Is this against guidelines?
+Maybe. [Read more](https://github.com/zkxs/NeosModLoader/blob/master/doc/neos_guidelines.md).
